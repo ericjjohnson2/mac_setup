a70f400947f6c343ebab6199890540fe74bfa17f
Eric Johnson <ericjohnson@Erics-Laptop.local>
2024-03-24 10:22:53 -0500

    added theme

--- vsc_extensions.sh
+++ vsc_extensions.sh
@@ -20,3 +20,6 @@ code --install-extension RDebugger.r-debugger
 code --install-extension julialang.language-julia
 code --install-extension cameronbieganek.julia-color-themes
 code --install-extension vscode-icons-team.vscode-icons
+
+# Theme installed but needs to be activated
+code --install-extension Equinusocio.vsc-material-theme
